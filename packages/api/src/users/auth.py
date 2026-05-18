7ea97b8bf0867bd0b1d69f1b299e7451038ed8ac
Kyrylo Triskalo <capitanx9@gmail.com>
2026-05-18 17:50:02 +0200

    Wire drf-spectacular with schema/swagger-ui URLs and CookieJWT extension

--- packages/api/src/users/auth.py
+++ packages/api/src/users/auth.py
@@ -1,6 +1,7 @@
 """DRF authentication class that reads the JWT from an HttpOnly cookie."""
 
 from django.conf import settings
+from drf_spectacular.extensions import OpenApiAuthenticationExtension
 from rest_framework.request import Request
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
@@ -12,3 +13,15 @@ class CookieJWTAuthentication(JWTAuthentication):
             return None
         validated_token = self.get_validated_token(raw_token)
         return self.get_user(validated_token), validated_token
+
+
+class CookieJWTSchema(OpenApiAuthenticationExtension):
+    target_class = "users.auth.CookieJWTAuthentication"
+    name = "cookieAuth"
+
+    def get_security_definition(self, auto_schema) -> dict:  # type: ignore[no-untyped-def, type-arg]
+        return {
+            "type": "apiKey",
+            "in": "cookie",
+            "name": settings.ACCESS_TOKEN_COOKIE,
+        }
